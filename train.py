2fe92c787780a5b0bf6174659b6871c58d2e04e7
SalmaGhrib <100800934+SalmaGhrib@users.noreply.github.com>
2026-09-20 20:12:31 +0200

    Update comment for GitHub Actions trigger test

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import shutil
 import tempfile
 import zipfile
 from pathlib import Path
-# Test du déclenchement GitHub Actions
+# Test du déclenchement GitHub Actions test2
 import joblib
 import numpy as np
 from PIL import Image
